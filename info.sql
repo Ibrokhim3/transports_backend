7bd4567dcb61ed9a530ea70173bc6b5b01e2164b
Ibrokhim3 <ishamasiddinov.97@gmail.com>
2023-04-28 21:35:14 +0500

    the db structure was created database created

--- info.sql
+++ info.sql
@@ -1,19 +1,45 @@
 CREATE DATABASE transports_db;
 
 CREATE TABLE staffs(
-  staff_id VARCHAR UNIQUE NOT NULL gen_random_uuid(),
+  staff_id VARCHAR UNIQUE NOT NULL DEFAULT gen_random_uuid(),
   username VARCHAR UNIQUE NOT NULL,
   birth_date DATE NOT NULL,
   gender VARCHAR(20) NOT NULL,
   branch VARCHAR REFERENCES branches(branch_id) ON DELETE CASCADE
-)
+);
 
 CREATE TABLE branches(
-  branch_id VARCHAR UNIQUE NOT NULL gen_random_uuid(),
+  branch_id VARCHAR UNIQUE NOT NULL DEFAULT gen_random_uuid(),
   branch_name VARCHAR UNIQUE NOT NULL,
   created_date TIMESTAMP(0) DEFAULT current_timestamp,
-  branch_address VARCHAR(100) NOT NULL,
-)
+  branch_address VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE transports(
+  transport_id  VARCHAR UNIQUE NOT NULL DEFAULT gen_random_uuid(),
+  model VARCHAR UNIQUE NOT NULL,
+  branch VARCHAR NOT NULL REFERENCES branches(branch_id) ON DELETE CASCADE,
+  color VARCHAR(50) NOT NULL,
+  created_date TIMESTAMP(0) DEFAULT current_timestamp
+);
+
+CREATE TABLE images(
+  image_id  VARCHAR UNIQUE NOT NULL DEFAULT gen_random_uuid(),
+  transport_id VARCHAR UNIQUE NOT NULL REFERENCES transports(transport_id) ON DELETE CASCADE,
+  image_url VARCHAR NOT NULL,
+  size NUMERIC
+);
+
+CREATE TABLE permissions(
+  staff_id VARCHAR UNIQUE NOT NULL REFERENCES staffs(staff_id) ON DELETE CASCADE,
+  create_p BOOLEAN DEFAULT false,
+  read_P BOOLEAN DEFAULT false,
+  update_p BOOLEAN DEFAULT false,
+  delete_p BOOLEAN DEFAULT false,
+  transport_module BOOLEAN DEFAULT false,
+  branch_module BOOLEAN DEFAULT false,
+  permission_module BOOLEAN DEFAULT false
+);
 
 
 
